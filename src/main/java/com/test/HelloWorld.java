42080b065b81deaf3daf6e2e2b6289ca7266a3a4
liu.yx <liu.yx@topcheer.com>
2020-04-06 11:55:26 +0800

    主线测试

--- src/main/java/com/test/HelloWorld.java
+++ src/main/java/com/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("分支代码");
+        System.out.println("主线代码");
     }
 }
