788babb5719512739ffc5a83b30da171c7bcbce9
lyx <1143629213@qq.com>
2022-02-24 22:10:26 +0800

    测试提交主分支代码

--- src/main/java/com/test/HelloWorld.java
+++ src/main/java/com/test/HelloWorld.java
@@ -2,8 +2,6 @@ package com.test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
-        System.out.println("分支代码");
-        System.out.println("主线代码");
+        System.out.println("测试提交主分支");
     }
 }
